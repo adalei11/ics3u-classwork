97ee2a9d7a6658b4c0b25db2ab3cd30dee4631e0
adalei11 <60791772+adalei11@users.noreply.github.com>
2020-11-20 13:19:23 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -2,3 +2,11 @@
 color = input('what is your favorite color? ')
 
 print(color,'?! No way, that is my favorite color as well!')
+
+#question 2
+cans = float(input('how many cans come in a pack? '))
+packs = float(input('how many packs are there? '))
+
+total_cans = cans * packs
+
+print('The total number of cans is ', total_cans)
