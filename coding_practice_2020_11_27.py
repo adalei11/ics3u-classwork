e04d4c52f3add4424168d1ebc5d0a3441d437102
adalei11 <60791772+adalei11@users.noreply.github.com>
2020-11-27 13:29:54 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,62 @@
+    # 1. from -482 to -118 by 14.
+    # 2. from -69 to 542 by 13.
+    # 3. from -37 to 167 by 12.
+    # 4. from -203 to 77 by 7.
+    # 5. from 496 to 1342 by 18.
+    # 6. from -28 to 703 by 17.
+    # 7. from -445 to -345 by 4.
+    # 8. from 384 to 618 by 9.
+    # 9. from -274 to 110 by 16.
+    # 10. from 125 to 251 by 6.
+    
+i = -482
+while i <= -118:
+    print(i)
+    i += 14
+
+
+i = -69
+while i <= 542:
+    print(i)
+    i += 13
+
+i = -37
+while i <= 167:
+    print(i)
+    i += 12
+
+i = -203
+while i <= 77 :
+    print(i)
+    i += 7
+
+i = 496
+while i <= 1342:
+    print(i)
+    i += 18
+
+i = -28
+while i <= 703:
+    print(i)
+    i += 17
+
+i = -445
+while i <= -345:
+    print(i)
+    i += 4
+
+i = 384
+while i <= 618:
+    print(i)
+    i += 9
+
+i = -274
+while i <= 110:
+    print(i)
+    i += 16
+
+i = 125
+while i <= 251:
+    print(i)
+    i += 6
+
